2930cf4e9e856af86e938dac1d92316c73657996
Ruth Poliakon <r.poliakon@gmail.com>
2019-12-10 17:39:12 -0700

    logger implementation

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code away!
+const express = require("express");
+
+const server = require("./server");
+
+const userRouter = require("./users/userRouter");
+
+server.use(express.json());
+server.use("/api/users", userRouter);
+
+server.listen(4000, () => {
+  console.log("\n*** Server Running on http://localhost:4000 ***\n");
+});
