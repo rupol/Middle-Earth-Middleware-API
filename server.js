21ae75221c6d32036861e459b4abd02d98bfaaa4
Ruth Poliakon <r.poliakon@gmail.com>
2019-12-12 18:19:03 -0700

    Endpoint ReadMe at root

--- server.js
+++ server.js
@@ -3,7 +3,51 @@ const express = require("express");
 const server = express();
 
 server.get("/", (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+  res.send(`
+  <h1>Middle Earth Middleware</h1>
+  <h2>User Endpoints</h2>
+  <table style="
+  width: 600px;
+  border: 1px solid black;
+  text-align: center;
+  ">
+    <tr>
+      <th>Endpoints</th>
+      <th>Request</th>
+    </tr>
+    <tr>
+      <td>/api/users</td>
+      <td>GET, POST</td>
+    </tr>
+    <tr>
+      <td>/api/users/:id</td>
+      <td>GET, PUT, DELETE</td>
+    </tr>
+    <tr>
+      <td>/api/users/:id/posts</td>
+      <td>GET, POST</td>
+    </tr>
+  </table>
+  <h2>Post Endpoints</h2>
+  <table style="
+  width: 600px;
+  border: 1px solid black;
+  text-align: center;
+  ">
+    <tr>
+      <th>Endpoints</th>
+      <th>Request</th>
+    </tr>
+    <tr>
+      <td>/api/posts</td>
+      <td>GET</td>
+    </tr>
+    <tr>
+      <td>/api/posts/:id</td>
+      <td>GET, PUT, DELETE</td>
+    </tr>
+  </table>
+  `);
 });
 
 //custom middleware
